ba1ab0aa9c4595ff44139c81a0fc26e8bb05fa78
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2020-12-05 11:58:46 +0100

    feat: adding EmailsEditor class

---
+++ src/classes/EmailsEditor.ts
@@ -0,0 +1,12 @@
+export class EmailsEditor {
+  email: string;
+
+  constructor(email: string){
+    this.email = email;
+  }
+
+  checkEmail(){
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regexp.test(this.email);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { EmailsEditor } from './classes/EmailsEditor';
+
 console.log('Hello World!');
 console.log('Hello Typescript!');
+
+const emailInput = document.getElementById("emailsInput") as HTMLInputElement;
+
+console.log(emailInput);
+
+const emailEditor = new EmailsEditor('gui@gmail.com');
+
+console.log(emailEditor.checkEmail());

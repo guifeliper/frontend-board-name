83bf3b766883f71b2db51978ab0bd674d2395517
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2020-12-05 16:15:59 +0100

    feat: creating container arround the functions

--- src/classes/EmailsEditor.ts
+++ src/classes/EmailsEditor.ts
@@ -6,13 +6,6 @@ export class EmailsEditor {
     this.email = email;
   }
 
-
-  private stringToHTML = function (str: string) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(str, 'text/html');
-    return doc.body;
-  };
-
   checkEmail(): Boolean {
     const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return regexp.test(this.email);
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,61 @@
 import { EmailsEditor } from './classes/EmailsEditor';
 import generateFakeEmail from './utils/generateFakeEmail';
 
-const emailInput = document.getElementById("emailsInput") as HTMLInputElement;
-const emailsContainer = document.getElementById("emailsContainer") as HTMLElement;
-const addEmail = document.getElementById("addEmail") as HTMLButtonElement;
-const getEmailsCount = document.getElementById("getEmails") as HTMLButtonElement;
+
+(function (container: HTMLElement, input: HTMLInputElement, buttonAdd: HTMLButtonElement, buttonGetEmail: HTMLButtonElement) {
+  // Functions 
+  function getValidEmails(): number {
+    const validEmails = container.getElementsByClassName('email');
+    return validEmails.length;
+  }
+
+  function generateEmail(emailEditor: EmailsEditor): void {
+    emailEditor.render(container);
+  }
+
+  function insertEmails(input: string): void {
+    var emailArr = input.trim().split(',').filter(x => x);
+    emailArr.forEach((email) => {
+      const emailEditor = new EmailsEditor(email.trim());
+      generateEmail(emailEditor);
+    });
+  }
 
 
-// Functions 
-function getValidEmails(): number {
-  const validEmails = document.getElementsByClassName('email');
-
-  return validEmails.length;
-}
-
-function generateEmail(emailEditor: EmailsEditor): void {
-  emailEditor.render(emailsContainer);
-}
-
-function insertEmails(input: string): void {
-  var emailArr = input.trim().split(',').filter(x => x);
-  emailArr.forEach((email) => {
-    const emailEditor = new EmailsEditor(email.trim());
-    generateEmail(emailEditor);
+  // ### Listeners
+  input.addEventListener('keyup', function (e) {
+    if (this.value.length < 3) return;
+    if (e.key === 'Enter' || e.key === ',') {
+      insertEmails(this.value);
+      this.value = '';
+    }
   });
-}
+
+  input.addEventListener('focusout', function (e) {
+    if (this.value.length < 3) return;
+    insertEmails(this.value);
+    this.value = '';
+  });
+
+  buttonGetEmail.addEventListener('click', function (e) {
+    let validEmailsCount: number = getValidEmails();
+    if (validEmailsCount > 1) {
+      alert(`You have ${validEmailsCount} valid emails`);
+    } else {
+      alert(`You have ${validEmailsCount} valid email`);
+    }
+  });
+
+  buttonAdd.addEventListener('click', function (e) {
+    const newEmail = generateFakeEmail();
+    insertEmails(newEmail);
+  });
 
 
 
-// ### Listeners
-emailInput.addEventListener('keyup', function (e) {
-  if (emailInput.value.length < 3) return;
-  if (e.key === 'Enter' || e.key === ',') {
-    insertEmails(emailInput.value);
-    emailInput.value = '';
-  }
-});
-
-emailInput.addEventListener('focusout', function (e) {
-  if (emailInput.value.length < 3) return;
-  insertEmails(emailInput.value);
-  emailInput.value = '';
-});
-
-getEmailsCount.addEventListener('click', function (e) {
-  let validEmailsCount: number = getValidEmails();
-  if (validEmailsCount > 1) {
-    alert(`You have ${validEmailsCount} valid emails`);
-  } else {
-    alert(`You have ${validEmailsCount} valid email`);
-  }
-});
-
-addEmail.addEventListener('click', function (e) {
-  const newEmail = generateFakeEmail();
-  insertEmails(newEmail);
-});
-
+})(document.getElementById("emailsContainer") as HTMLElement,
+  document.getElementById("emailsInput") as HTMLInputElement,
+  document.getElementById("addEmail") as HTMLButtonElement,
+  document.getElementById("getEmails") as HTMLButtonElement);
 
 // guilherme@gmail.com, mayara, karina@ghu.com, may@maracaja.com

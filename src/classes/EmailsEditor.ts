06fbd4b12e8fb8d5220a3c52074ba3a91d9f3fc7
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2020-12-05 12:47:18 +0100

    feat: rendering element

--- src/classes/EmailsEditor.ts
+++ src/classes/EmailsEditor.ts
@@ -1,12 +1,35 @@
 export class EmailsEditor {
   email: string;
 
-  constructor(email: string){
+  constructor(email: string) {
     this.email = email;
   }
 
-  checkEmail(){
+
+  private createElementFromHTML(htmlString: string) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
+  }
+  private stringToHTML = function (str: string) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(str, 'text/html');
+    return doc.body;
+  };
+
+  checkEmail(): Boolean {
     const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return regexp.test(this.email);
+  }
+
+  render(container: HTMLElement): void {
+    const htmlTemplate = `<span class="email">${this.email}
+                            <a class="js-delete-tag" title="Remove tag">
+                              x
+                            </a>
+                          </span>`;
+
+    const body = this.stringToHTML(htmlTemplate);
+    container.append(body);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { EmailsEditor } from './classes/EmailsEditor';
 
-console.log('Hello World!');
-console.log('Hello Typescript!');
-
 const emailInput = document.getElementById("emailsInput") as HTMLInputElement;
+const emailsContainer = document.getElementById("emailsContainer") as HTMLElement;
 
-console.log(emailInput);
 
-const emailEditor = new EmailsEditor('gui@gmail.com');
+emailInput.addEventListener('keypress', function (e) {
+  e.preventDefault();
 
-console.log(emailEditor.checkEmail());
+  var emailArr = emailInput.value.split(',');
+  console.log(emailArr);
+  if (e.key === 'Enter') {
+    var emailArr = emailInput.value.split(',');
+    emailArr.forEach((email)=> {
+      const emailEditor = new EmailsEditor(email.trim());
+      if(emailEditor.checkEmail()){
+        emailEditor.render(emailsContainer);
+      }
+      // console.log('Add email', emailEditor.checkEmail());
+    });
+
+  } else if(e.key === ','){
+    const emailEditor = new EmailsEditor(emailInput.value);
+    console.log('Add email', emailEditor.checkEmail());
+  }
+});
+// guilherme@gmail.com, mayara, karina@ghu.com, may@maracaja.com
